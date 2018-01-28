3eaa408a3b052ac36fd61e3ce0c234e996554791
hideki takeuchi <takeuchihidekijp@yahoo.co.jp>
2018-01-28 17:46:49 +0900

    カメラ切り替え

---
+++ Assets/Script/CameraSelect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSelect : MonoBehaviour {
+
+    private GameObject MainCam;
+    private GameObject StCam;
+
+	// Use this for initialization
+	void Start () {
+
+        MainCam = GameObject.Find("Main Camera");
+        StCam = GameObject.Find("StorategyCamera");
+
+        StCam.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            if (MainCam.activeSelf)
+            {
+                MainCam.SetActive(false);
+                StCam.SetActive(true);
+            }
+            else
+            {
+                MainCam.SetActive(true);
+                StCam.SetActive(false);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Script/CaracterSet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaracterSet : MonoBehaviour {
+
+    public bool zooming;
+    public float zoomSpeed;
+    public Camera camera;
+
+    public GameObject Caracter;
+
+    private Vector3 ClickPosition;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
