b8174967d092d53e3636d39c68d3010149849e7a
hideki takeuchi <takeuchihidekijp@yahoo.co.jp>
2018-01-28 21:32:43 +0900

    メンタリング前

--- Assets/Script/CaracterSet.cs
+++ Assets/Script/CaracterSet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CaracterSet : MonoBehaviour {
 
+
     public bool zooming;
     public float zoomSpeed;
     public Camera camera;
@@ -20,6 +21,25 @@ public class CaracterSet : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Camera camera = GetComponent<Camera>();
+
+            // Vector3でマウスがクリックした位置座標を取得する
+            ClickPosition = Input.mousePosition;
+
+            ClickPosition.z = 5f;
+
+            Vector3 instancePos = camera.ScreenToWorldPoint(ClickPosition);
+            //落ちないY座標を指定
+            instancePos.y = 1;
+
+            // オブジェクト生成 : オブジェクト(GameObject), 位置(Vector3), 角度(Quaternion)
+            // ScreenToWorldPoint(位置(Vector3))：スクリーン座標をワールド座標に変換する
+            Instantiate(Caracter, instancePos, Caracter.transform.rotation);
+        }
+
+    }
 }
