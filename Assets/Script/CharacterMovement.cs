cdbf2abde39139ca3f99c0fa67f4b56f6d4552ea
hideki takeuchi <takeuchihidekijp@yahoo.co.jp>
2018-02-01 09:32:03 +0900

    Navmesh作成

--- Assets/Script/CharacterMovement.cs
+++ Assets/Script/CharacterMovement.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
 
+    Vector3 movement;
+    Rigidbody characterRigidbody;
+
+    UnityEngine.AI.NavMeshAgent nav;
+
+    //移動ポイントのオブジェクト用List（キャラクターの移動ポイントをMovePointとする）
+    private int MovePointCount = 5;
+    private List<GameObject> movepointList = new List<GameObject>();
+
 	// Use this for initialization
 	void Start () {
+
+        for (int i = 1; i <= MovePointCount; ++i)
+        {
+            GameObject movepoint = GameObject.Find("MovePoint" + i.ToString());
+
+            if(movepoint != null)
+            {
+                movepointList.Add(movepoint);
+            }
+        }
+
+        characterRigidbody = GetComponent<Rigidbody>();
+
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        CharacterMoveMent();
 		
 	}
+
+    private void CharacterMoveMent()
+    {
+        int r = Random.Range(0, movepointList.Count);
+
+        nav.SetDestination(movepointList[r].transform.position);
+    }
 }
