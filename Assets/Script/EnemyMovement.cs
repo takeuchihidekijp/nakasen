8b8bb4c64dd6bad98bbecb7042f80e64be4a88f9
hideki takeuchi <takeuchihidekijp@yahoo.co.jp>
2018-02-06 09:30:48 +0900

    Enemy作成

---
+++ Assets/Script/EnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    Vector3 movement;
+    Rigidbody characterRigidbody;
+
+    UnityEngine.AI.NavMeshAgent nav;
+
+    [SerializeField] float m_interval = 1.5f;
+    float m_timer;
+
+    // Use this for initialization
+    void Start () {
+        characterRigidbody = GetComponent<Rigidbody>();
+
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //  CharacterMoveMent();
+        m_timer += Time.deltaTime;
+
+        if (m_timer > m_interval)
+        {
+            m_timer = 0f;
+            SetRandomDestination();
+        }
+
+    }
+
+    void SetRandomDestination()
+    {
+        nav.SetDestination(new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(10.0f, -10.0f)));
+    }
+}
---
+++ Assets/Script/EnemySet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySet : MonoBehaviour {
+
+    public GameObject prefab;
+
+    [SerializeField] float m_interval = 1.5f;
+    float m_timer;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_timer += Time.deltaTime;
+
+        if (m_timer > m_interval)
+        {
+            m_timer = 0f;
+            CreateEnemy();
+        }
+
+    }
+
+
+    void CreateEnemy()
+    {
+        Vector3 pos = new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(10.0f, 20.0f));
+
+        Instantiate(prefab, pos, Quaternion.identity);
+    }
+
+}
