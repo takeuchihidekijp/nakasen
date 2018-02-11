1b2c4b72f085c17e022f97b2446c17d0ad724254
hideki takeuchi <takeuchihidekijp@yahoo.co.jp>
2018-02-11 20:59:26 +0900

    Playerのグラフィック変更と索敵ロジック実装。

---
+++ Assets/Script/Finder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Finder : MonoBehaviour {
+
+    private List<GameObject> m_target = new List<GameObject>();
+
+    private void Awake()
+    {
+
+        var searching = GetComponentInChildren<SearchingBehavior>();
+        searching.onFound += OnFound;
+        searching.onLost += OnLost;
+    }
+
+    private void OnFound (GameObject i_foundObject)
+    {
+        Debug.Log("OnFound");
+    }
+
+    private void OnLost(GameObject i_lostObject)
+    {
+        Debug.Log("OnLost");
+    }
+}
---
+++ Assets/Script/GameData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//MonoBehaviourはClassの特性上取る
+public class GameData {
+
+    //敵の数の上限
+    public static int NUMBER_OF_ENEMYS = 15;
+
+    //Characterの数の上限
+    public static int NUMBER_OF_CHARACTERS = 15;
+		
+	
+}
