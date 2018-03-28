4a1849580fc49d2f29f5d29e85984472bfd1685c
hideki takeuchi <takeuchihidekijp@yahoo.co.jp>
2018-03-29 00:54:11 +0900

    通常キャラと捕虜解放キャラの切り替えスイッチの実装。UIを大きくして見やすくする。

---
+++ Assets/Script/ToggleCharacterChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleCharacterChange : MonoBehaviour {
+
+    Toggle toggle;
+
+    public bool normal_flg = true;
+
+	// Use this for initialization
+	void Start () {
+        toggle = GetComponent<Toggle>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeToggle()
+    {
+        Debug.Log("Toggleが変更されました");
+
+        if(this.toggle.isOn == true)
+        {
+            normal_flg = true;
+            Debug.Log("Toggleが変更されました normal_flg = true");
+        }
+        else
+        {
+            normal_flg = false;
+            Debug.Log("Toggleが変更されました normal_flg = false");
+        }
+    }
+}
